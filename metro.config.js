67e0f6c7fa14f7980b1864ad643b3fc4fae73039
taka37negi-coder <taka37negi@gmail.com>
2026-02-25 03:02:18 +0900

    Clean up comments in metro.config.js
    Removed comments explaining forceWriteFileSystem option.

--- metro.config.js
+++ metro.config.js
@@ -10,7 +10,5 @@ config.resolver.blockList = [
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
   forceWriteFileSystem: true,
 });
